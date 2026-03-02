7b8b6e32a608df122d1e65245638573924645603
arpit-upadhyay-68 <arpit68upadhyay@gmail.com>
2026-03-02 10:02:14 +0530

    [arpit upadhyay] Added [UC1-Application Entry & Welcome Message]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class PalindromeCheckerApp {
+
+        public static void main(String[] args) {
+            // Application Entry Point
+
+            displayWelcomeMessage();
+
+            // Next use case can be called here
+            // For now, we just exit
+        }
+
+        private static void displayWelcomeMessage() {
+            System.out.println("=================================");
+            System.out.println("     Palindrome Checker App      ");
+            System.out.println("=================================");
+            System.out.println("Version: 1.0.0");
+            System.out.println("Author: Your Name");
+            System.out.println("---------------------------------");
+            System.out.println("This application checks whether");
+            System.out.println("a given string is a palindrome.");
+            System.out.println("=================================");
+        }
+    }
+
