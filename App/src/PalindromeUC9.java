2967a6736710e7fda90528b253d3b037da56c4e7
chayan bisai <chayanbisai43@example.com>
2026-03-10 10:10:19 +0530

    [arpit upadhyay] added UC10-insensitivepalindtrome

--- App/src/PalindromeUC9.java
+++ App/src/PalindromeUC9.java
@@ -1,4 +1,4 @@
-public class PalindromeUC9 {
+public class PalindromeUC10 {
 
     // Recursive function to check palindrome
     public static boolean isPalindrome(String str, int start, int end) {
@@ -8,20 +8,19 @@ public class PalindromeUC9 {
         }
 
         // If characters at start and end don't match
-        if (str.charAt(start) != str.charAt(end))
-        {
+        if (str.charAt(start) != str.charAt(end)) {
             return false;
         }
 
-        // Recursive call for next inner substring
+        // Recursive call for the next inner characters
         return isPalindrome(str, start + 1, end - 1);
     }
 
-    public static void main(String[] args)
-    {
-        String word = "ADA";
+    public static void main(String[] args) {
+        // Hardcoded string
+        String word = "121";
 
-        // Call recursive function
+        // Check palindrome using recursion
         if (isPalindrome(word, 0, word.length() - 1)) {
             System.out.println("The string \"" + word + "\" is a Palindrome.");
         } else {
